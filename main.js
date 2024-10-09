1dedf268847dd03b6cee704c7d0d09d4dddabc57
Zahra Karimi <thezahrakarimi1@gmail.com>
2024-10-09 11:39:44 -0600

    Changes

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 
-// Get the button
 const topButton = document.getElementById('topButton');
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         topButton.style.display = "flex";
